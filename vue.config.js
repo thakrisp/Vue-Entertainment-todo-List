4b0d01493f09c2ed5791eb97796637bfcf9acb53
Thakrisp <ryancrispin@gmail.com>
2021-03-13 22:35:54 -0500

    more deploy test

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,11 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, './server/public'),
-  pluginOptions: {
-    express: {
-      shouldServeApp: true,
-      serverDir: './server',
-    },
-  },
   devServer: {
-    proxy: 'http://localhost:5000',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+    },
   },
 };
