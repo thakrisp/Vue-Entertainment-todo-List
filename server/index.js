d406366627a701b83acb4d467f3039087cfe9ff9
Thakrisp <ryancrispin@gmail.com>
2021-03-13 23:46:37 -0500

    another deploy test

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'));
 
   // Handle SPA
-  console.log(__dirname + '/public/index.html');
+  //console.log(__dirname + '/public/index.html');
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
