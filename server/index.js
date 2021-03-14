068b7ef09bcf965360a7051e96d5dd249c462d8e
Thakrisp <ryancrispin@gmail.com>
2021-03-14 16:25:30 -0400

    even more deploy tests

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const posts = require('./routes/api/items');
+const posts = require('../server/routes/api/items');
 
 app.use('/api/items', posts);
 
