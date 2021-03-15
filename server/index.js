778ea5fdb08a9ac0818eec4da4efa7f3f463e545
Thakrisp <ryancrispin@gmail.com>
2021-03-14 21:16:22 -0400

    even more deploy tests

--- server/index.js
+++ server/index.js
@@ -16,10 +16,8 @@ if (process.env.NODE_ENV === 'production') {
   // Static folder
   app.use(express.static(__dirname + '/public/'));
 
-  app.use('/api/items', posts);
-
   // Handle SPA
-  app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  //app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
