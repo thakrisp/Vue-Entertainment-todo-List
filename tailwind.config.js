7d5d62993a15e8f983c90aec9dcf5a2718818429
Thakrisp <ryancrispin@gmail.com>
2021-03-25 15:54:55 -0400

    purge unused CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/assets/css/tailwind.css'],
+  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
